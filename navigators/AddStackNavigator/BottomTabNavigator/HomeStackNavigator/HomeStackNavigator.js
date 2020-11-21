af2bf2a71a47616529b41c9f0c81181d4c7090ad
lakshanss <lakshan230897@gmail.com>
2020-11-21 14:29:57 +0530

    Change HomeStackNavigator

--- navigators/AddStackNavigator/BottomTabNavigator/HomeStackNavigator/HomeStackNavigator.js
+++ navigators/AddStackNavigator/BottomTabNavigator/HomeStackNavigator/HomeStackNavigator.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import {createStackNavigator} from "@react-navigation/stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
+import NewHomeScreen from "./screens/NewHomeScreen";
+import ProductPage from "./screens/ProductPage";
 
 const HomeStack = createStackNavigator();
 
 function HomeStackNavigator() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-    </HomeStack.Navigator>
-  );
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen options={{headerShown: false}}  name="New Home" component={NewHomeScreen}/>
+            <HomeStack.Screen name="Details" component={DetailsScreen}/>
+            <HomeStack.Screen name="Home" component={HomeScreen}/>
+            <HomeStack.Screen name="Product" component={ProductPage}/>
+        </HomeStack.Navigator>
+    );
 }
 
 export default HomeStackNavigator;
