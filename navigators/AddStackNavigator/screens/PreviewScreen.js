9339b4ef188333b591ee7224f70e187538599b66
lakshanss <lakshan230897@gmail.com>
2020-12-06 21:30:54 +0530

    Fix preview screen

--- navigators/AddStackNavigator/screens/PreviewScreen.js
+++ navigators/AddStackNavigator/screens/PreviewScreen.js
@@ -1,52 +1,53 @@
 import React from "react";
-import {
-  Text,
-  View,
-  Image,
-  StyleSheet,
-  useWindowDimensions,
-  TouchableOpacity,
-  ImageBackground,
-} from "react-native";
-import { Button } from "@ui-kitten/components";
+import {View, StyleSheet, ImageBackground, Dimensions} from "react-native";
+import {Button} from "@ui-kitten/components";
 import Constants from "expo-constants";
+import {Avatar} from "react-native-elements";
 
-export default function PreviewScreen({ navigation, route }) {
-  const { photo } = route.params;
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
-  console.log(photo);
-  return (
-    <View style={{ flex: 1 }}>
-      <ImageBackground style={{ flex: 1 }} source={{ uri: photo.uri }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row",
-          }}
-        >
-          <View style={{ marginTop: 50, marginLeft: 10 }}>
-            <Button onPress={() => navigation.goBack()}>Retake</Button>
-          </View>
-          <View
-            style={{
-              marginTop: 720,
-              marginLeft: 140,
-            }}
-          >
-            <Button>Use</Button>
-          </View>
+export default function PreviewScreen({navigation, route}) {
+    const {photo} = route.params;
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
+    const camViewHeight = (screenWidth / 22) * 16;
+    console.log(photo);
+    return (
+        <View style={{paddingTop: 60, height: screenHeight, backgroundColor: "black"}}>
+            <Avatar
+                rounded
+                size="medium"
+                icon={{name: 'chevron-left', type: 'font-awesome', color: "white"}}
+                activeOpacity={0.7}
+                onPress={() => navigation.goBack()}
+            />
+            <ImageBackground style={{
+                width: screenWidth, marginTop: 50,
+                height: camViewHeight
+            }} source={{uri: photo.uri}}>
+            </ImageBackground>
+            <View
+                style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginTop: 30,
+                    marginBottom: 20
+                }}
+            >
+                <View style={{width: "33%", alignItems: "center"}}>
+                </View>
+                <View style={{width: "33%", alignItems: "center"}}>
+                    <Button style={{backgroundColor: "black", borderColor: "white"}}>Use Image</Button>
+                </View>
+                <View style={{width: "33%"}}>
+                </View>
+            </View>
         </View>
-      </ImageBackground>
-    </View>
-  );
+    );
 }
 
 const formStyling = StyleSheet.create({
-  layoutPadding: {
-    paddingTop: Constants.statusBarHeight + 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
+    layoutPadding: {
+        paddingTop: Constants.statusBarHeight + 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
 });
